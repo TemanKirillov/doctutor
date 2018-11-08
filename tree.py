1061689ebc9a2c9b2fa8bd4c72831fc8395385a6
Teman Kirillov <kirillov.email@yandex.ru>
2018-11-08 04:42:48 +0300

    Из функции ownattr выделена функция isinparent

--- tree.py
+++ tree.py
@@ -6,27 +6,34 @@
 class I:
     import inspect
 
-
 def isdescriptor(obj):
     ''' Реализован ли в объекте протокол дескриптора?'''
     if hasattr(obj, '__get__') or hasattr(obj, '__set__'):
         return True
     return False
 
+
+def isinparent(obj, nameattr):
+    ''' Наследован ли атрибут от родителя.
+        Принимает объект и имя его атрибута'''
+    cls = obj.__class__
+    objattr = getattr(obj, nameattr)
+    memcls = dict(I.inspect.getmembers(cls))
+
+    if nameattr in memcls:
+        parent_obj = memcls[nameattr]
+        if (parent_obj == objattr or isdescriptor(parent_obj)):
+            return True
+    return False
+
 def ownattr(obj):
     ''' Генераторная функция. Производит атрибуты объекта в стиле inspect.getmembers, которые принадлежат самому объекту. '''
 
     memobj = I.inspect.getmembers(obj)
-    cls = obj.__class__
-    memcls = dict(I.inspect.getmembers(cls))
 
     for namemem, objmem in memobj:
-        if namemem in memcls:
-            parent_obj = memcls[namemem]
-            if (parent_obj == objmem or isdescriptor(parent_obj)):
-                pass
-            else:
-                yield namemem, objmem
+        if isinparent(obj, namemem):
+            pass
         else:
             yield namemem, objmem
 
