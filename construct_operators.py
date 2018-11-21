33eba4177aaba21a34e3a715c3e0d566718c9bcb
TemanKirillov <kirillov.email@yandex.ru>
2018-11-21 17:28:35 +0300

    Продолжаю тестирование not_own_dunder

--- construct_operators.py
+++ construct_operators.py
@@ -10,9 +10,13 @@ def pred_own_dunder(*args):
 own_dunder = I.tree.getmembers('string.Template', I.string.Template, pred_own_dunder) 
 print("==============own_dunder++++++++++++++")
 [print(i[0]) for i in list(own_dunder)]
+print("==============impdunder++++++++++++++")
 def pred_imp_dunder(*args):
     return I.tree.isdunder(*args) and I.tree.isimp(*args)
 imp_dunder = I.tree.getmembers('string.Template', I.string.Template, pred_imp_dunder) 
-print("==============impdunder++++++++++++++")
 [print(i[0]) for i in list(imp_dunder)]
-
+print("==============not_own_dunder++++++++++++++")
+def pred_not_own_dunder(*args):
+    return not pred_own_dunder(*args) and I.tree.isdunder(*args)
+not_own_dunder = I.tree.getmembers('string.Template', I.string.Template, pred_not_own_dunder) 
+[print(i[0]) for i in list(not_own_dunder)]
