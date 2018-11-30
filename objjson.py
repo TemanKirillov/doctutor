1d3c248c37a833f4737ae3e0987788d0ceb43286
TemanKirillov <kirillov.email@yandex.ru>
2018-11-30 15:34:30 +0300

    Функция objjson.object_hook

--- objjson.py
+++ objjson.py
@@ -2,10 +2,25 @@ class I:
     import obj
     import json
     import pprint
+    import repr
 
-obj = I.obj.Obj()
-obj.a = 'test a'
-obj.b = 'test b'
+def object_hook(obj):
+    dct = dict(obj)
+    cls = dict
+    if '_type' in dct:
+        _type = dct['_type']
+        if _type in I.obj.__dict__:
+            cls = I.obj.__dict__[_type]
+    return cls(obj)
+
+obj = I.obj.Params()
+obj.a = I.obj.Param.from_iterable(['param1', 'POSITION', '3', 'Param1 for test'])
+obj.b = I.obj.Param.from_iterable(['param2', 'POSITION', '"string"', 'Param2 for test'])
 
 with open('test_json.txt', mode='w') as file:
     I.json.dump(obj, file, indent=4, separators=(',\n', ': '))
+
+with open('test_json.txt') as file:
+    obj2 = I.json.load(file, object_hook=object_hook)
+
+print(obj2)
