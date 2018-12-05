c57578fdace4cce91d618016b8b534504d9cfb18
TemanKirillov <kirillov.email@yandex.ru>
2018-12-05 11:18:50 +0300

    Вливание tree.py из master, переименование в myinspect.py

--- myinspect.py
+++ myinspect.py
@@ -7,20 +7,7 @@ class I:
     import inspect
     import re
     from contextlib import AbstractContextManager
-
-class AttributeException:
-    'Класс для представления атрибута, обращаясь к которому возникает исключение'
-    def __init__(self, exc):
-        self.exc = exc
-    def __repr__(self):
-        namecls = self.__class__.__name__
-        return '{cls}({exc})'.format(namecls,self.exc) 
-    @classmethod
-    def make(cls, obj, nameattr):
-        try:
-            objattr = getattr(obj, nameattr)
-        except Exception as e:
-            return cls(e)
+    import builtins
 
 def to_short(qualname):
     ''' Преобразовать квалифицированное имя в короткое.''' 
@@ -48,6 +35,34 @@ class ContextPatterns(I.AbstractContextManager):
         qualname = '.'.join([name, nameattr])
         return any_fullmatch(qualname, *self.patterns)
 
+def getattr(obj, nameattr):
+    ''' Реализация getattr для работы с атрибутами, которые возбуждают AttributeError при обращении к себе. '''
+    if I.inspect.isclass(obj):
+        mro = (obj,) + I.inspect.getmro(obj)
+    else:
+        mro = ()
+    try:
+        value = I.builtins.getattr(obj, nameattr)
+    except AttributeError as e:
+        for base in mro:
+            if nameattr in base.__dict__:
+                value = base.__dict__[key]
+                break
+        else:
+            raise e
+
+    return value
+
+def getmembers(name, obj, predicate):
+    ''' Генератор. Реализует другой тип предиката для inspect.getmembers. Выталкивает кортежи (имя, объект) из объектов и его атрибутов.
+        predicate - функция с сигнатурой (name, obj, nameattr): принимает имя объекта, объект и имя его атрибута. Возвращает True, если атрибут должен быть вытолкнут. ''' 
+
+    members = I.inspect.getmembers(obj)
+
+    for namemem, objmem in members:
+        if predicate(name, obj, namemem):
+            yield namemem, objmem
+
 def getmembers_recursive(name, obj, predicate, predicate_recursive):
     ''' Генераторная функция. Реализует рекурсивный обход атрибутов объекта. 
     name: имя объекта,
@@ -59,6 +74,35 @@ def getmembers_recursive(name, obj, predicate, predicate_recursive):
         yield qualname, objattr
         if predicate_recursive(name, obj, nameattr):
             yield from getmembers_recursive(qualname, objattr, predicate, predicate_recursive)
+
+def getparent(obj, nameattr):
+    ''' Возвращает объект, в котором определён атрибут nameattr объекта obj'''
+    if I.inspect.isclass(obj):
+        loc = I.inspect.classify_class_attrs(obj)
+        memcls = {attr.name: attr for attr in loc}
+        if nameattr in memcls:
+            return memcls[nameattr].defining_class
+        else:
+            raise AttributeError('Object {!r} has no attribute {!r}'.format(obj, nameattr))
+    else:
+        cls = obj.__class__
+        objattr = getattr(obj, nameattr)
+        memcls = dict(I.inspect.getmembers(cls))
+
+        if nameattr in memcls:
+            parent_obj = memcls[nameattr]
+            if (parent_obj == objattr or isdescriptor(parent_obj)):
+                return getparent(cls, nameattr)
+            else:
+                return obj #атрибут переопределён в экземпляре
+        return obj #атрибут определён в экземпляре
+
+def getparents(obj):
+    ''' Возвращает предков объекта, как и inspect.getmro, но не только для классов'''
+    if I.inspect.isclass(obj):
+        return I.inspect.getmro(obj)
+    else:
+        return (obj,) + I.inspect.getmro(obj.__class__)
 
 def isdescriptor(obj):
     ''' Реализован ли в объекте протокол дескриптора?'''
@@ -72,19 +116,16 @@ def isdunder(name, obj, nameattr):
 def isinparent(name, obj, nameattr):
     ''' Наследован ли атрибут от родителя.
         Принимает объект и имя его атрибута'''
-    cls = obj.__class__
-    objattr = getattr(obj, nameattr)
-    memcls = dict(I.inspect.getmembers(cls))
-
-    if nameattr in memcls:
-        parent_obj = memcls[nameattr]
-        if (parent_obj == objattr or isdescriptor(parent_obj)):
-            return True
-    return False
+    if getparent(obj, nameattr) is obj:
+        return False
+    else:
+        return True
 
 def isimp(name, obj, nameattr):
     ''' Импортирован ли атрибут?
         Принимает объект и имя его атрибута'''
+    if isinparent(name, obj, nameattr):
+        return False
     objattr = getattr(obj, nameattr)
     module = I.inspect.getmodule(obj)
     modattr = I.inspect.getmodule(objattr)
@@ -96,38 +137,13 @@ def isimp(name, obj, nameattr):
         else:
             return False
     
-def isattrexception(name, obj, nameattr):
-    try:
-        objattr = getattr(obj, nameattr)
-    except Exception as e:
-        return True
-    return False
-
-def getattr(obj, nameattr):
-    ''' Реализация getattr для работы с атрибутами, которые возбуждают AttributeError при обращении к себе. '''
-    dct = dict(I.inspect.getmembers(obj))
-    return dct[nameattr]
-
 def ownattr(name, obj):
     ''' Генераторная функция. Производит атрибуты объекта в стиле inspect.getmembers, которые принадлежат самому объекту. '''
 
-    memobj = I.inspect.getmembers(obj)
+    def predicate(*args):
+        return not(isinparent(*args) or isimp(*args))
+    yield from getmembers(name, obj, predicate)
 
-    for namemem, objmem in memobj:
-        if isinparent(name, obj, namemem) or isimp(name, obj, namemem):
-            pass
-        else:
-            yield namemem, objmem
-
-def getmembers(name, obj, predicate):
-    ''' Генератор. Реализует другой тип предиката для inspect.getmembers. Выталкивает кортежи (имя, объект) из объектов и его атрибутов.
-        predicate - функция с сигнатурой (name, obj, nameattr): принимает имя объекта, объект и имя его атрибута. Возвращает True, если атрибут должен быть вытолкнут. ''' 
-
-    members = I.inspect.getmembers(obj)
-
-    for namemem, objmem in members:
-        if predicate(name, obj, namemem):
-            yield namemem, objmem
 
 if __name__ == '__main__':
     pass
---
+++ test_myinspect.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python3 
+
+class I:
+    'imported objects'
+    import unittest
+    import string
+    from tree import ownattr
+    from tree import any_fullmatch
+    from tree import getmembers
+    from tree import isimp
+    from tree import isinparent
+    from tree import ContextPatterns
+    from tree import getmembers_recursive
+    from tree import isdunder
+    from tree import getparent
+    from tree import getparents
+
+class Test_ownattr(I.unittest.TestCase):
+
+    def test_builtin_func(self):
+        gen = I.ownattr('hex', hex)
+        lst = list(dict(gen))
+        self.assertEqual(len(lst), 4)
+        self.assertListEqual(lst, ['__class__', '__name__', '__qualname__', '__text_signature__'])
+
+    def test_user_func(self):
+        def test(a,b):
+            ''' test function'''
+        test.attr = 8
+        lst = list(dict(I.ownattr('test', test)))
+        self.assertEqual(len(lst), 6)
+        self.assertListEqual(lst, ['__dict__', '__doc__', '__module__', '__name__', '__qualname__', 'attr'])
+
+    def test_class(self):
+        class Test:
+            ''' test class'''
+            a = 8
+            def __init__(self):
+                self.b = 22
+            def method(self):
+                pass
+
+        lst = list(dict(I.ownattr('Test', Test)))
+        self.assertEqual(len(lst), 7)
+        self.assertListEqual(lst, ['__dict__', '__doc__', '__init__', '__module__', '__weakref__', 'a', 'method'])
+
+        inst = Test()
+        inst.attr = 'test attr'
+        lst = list(dict(I.ownattr('Test', inst)))
+        self.assertEqual(len(lst), 4)
+        self.assertListEqual(lst, ['__class__', '__dict__', 'attr', 'b'])
+
+    def test_module(self):
+        lst = list(dict(I.ownattr('string', I.string)))
+        self.assertEqual(len(lst), 22)
+        self.assertListEqual(lst, ['Formatter', 'Template', '_TemplateMetaclass', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'ascii_letters', 'ascii_lowercase', 'ascii_uppercase', 'capwords', 'digits', 'hexdigits', 'octdigits', 'printable', 'punctuation', 'whitespace',] 
+        )
+
+class Test_any_fullmatch(I.unittest.TestCase):
+    def test_all(self):
+        patterns = ['a', '\d\d', '.+__']
+        self.assertTrue(I.any_fullmatch('a', *patterns))
+        self.assertTrue(I.any_fullmatch('23', *patterns))
+        self.assertFalse(I.any_fullmatch('b', *patterns))
+        self.assertFalse(I.any_fullmatch('235', *patterns))
+
+class Test_getmembers(I.unittest.TestCase):
+    def test_isimp(self):
+        lst = list(dict(I.getmembers('string', I.string, I.isimp)))
+        self.assertListEqual(lst, ['_ChainMap','_re', '_string' ] )
+
+class Test_ContextPatterns(I.unittest.TestCase):
+    def test_sn(self):
+        def f1():
+            pass
+        f1.a25 = 3
+        with I.ContextPatterns(r'a\d\d', r'__\w\w') as context:
+            func = context.match_nameattr
+            self.assertTrue(func('f1', f1, 'a25'))
+            self.assertFalse(func('f1', f1, 'abc'))
+            self.assertTrue(func('f1', f1, '__ab'))
+            self.assertFalse(func('f1', f1, '__ab__'))
+
+    def test_sn_getmembers(self):
+        def f1():
+            pass
+        f1.a25 = 3
+        f1.abc = 3
+        f1.__ab = 3
+        f1.__ab__ = 3
+        with I.ContextPatterns(r'a\d\d', r'__\w\w') as context:
+            gen = I.getmembers('f1', f1, context.match_nameattr)
+            self.assertListEqual(list(gen), [('a25', 3)])
+
+    def test_qn(self):
+        def f1():
+            pass
+        f1.a25 = 3
+        with I.ContextPatterns(r'f1[.]a\d\d', r'f2[.]__\w\w') as context:
+            func = context.match_qualname
+            self.assertTrue(func('f1', f1, 'a25'))
+            self.assertFalse(func('f1', f1, 'abc'))
+            self.assertFalse(func('f1', f1, '__ab'))
+            self.assertTrue(func('f2', f1, '__ab'))
+
+class Test_getmembers_recursive(I.unittest.TestCase):
+    
+    def test_isimp(self):
+        # извлечение импортированных элементов рекурсивно
+        def temp(name, obj, nameattr): #чтобы избежать зацикливания на внутренних атрибутах
+            return I.isimp(name, obj, nameattr) and not I.isdunder(name, obj, nameattr)
+        def all_(*args):
+            return True
+        gen = I.getmembers_recursive('string', I.string, temp, all_ ) 
+        st = set(dict(gen))
+        self.assertEqual(len(st), 32)
+        example = {'string._ChainMap', 'string._re', 'string._re._locale', 'string._re._locale.Error', 'string._re._pattern_type', 'string._re.copyreg', 'string._re.enum', 'string._re.enum.DynamicClassAttribute', 'string._re.enum.MappingProxyType', 'string._re.enum.OrderedDict', 'string._re.enum._or_', 'string._re.enum.reduce', 'string._re.enum.sys', 'string._re.enum.sys.excepthook', 'string._re.error', 'string._re.functools', 'string._re.functools.MappingProxyType', 'string._re.functools.RLock', 'string._re.functools.WeakKeyDictionary', 'string._re.functools.cmp_to_key', 'string._re.functools.get_cache_token', 'string._re.functools.namedtuple', 'string._re.functools.recursive_repr', 'string._re.functools.reduce', 'string._re.sre_compile', 'string._re.sre_compile._sre', 'string._re.sre_compile.error', 'string._re.sre_compile.sre_parse', 'string._re.sre_compile.sre_parse.error', 'string._re.sre_parse', 'string._re.sre_parse.error', 'string._string',}
+        self.assertSetEqual(example, st)
+
+    def test_ownattr(self):
+        #тестирование извлечения собственных аргументов 
+        def temp(*args):
+            return I.isimp(*args) or I.isdunder(*args) or I.isinparent(*args)
+
+        def temp_not(*args):
+            return not temp(*args)
+
+        def all_(*args):
+            return True
+        def not_parent(*args):
+            return not I.isinparent(*args)
+        
+        gen = I.getmembers_recursive('string', I.string, not_parent, temp_not ) 
+        lst = list(gen)
+        self.assertEqual(len(lst), 178)
+        # print('Вывод собственных аргументов модуля string рекурсивно')
+        # [print(i[0]) for i in lst]
+
+class Test_getparent(I.unittest.TestCase):
+    
+    def test(self):
+        class A:
+            a = 1
+        class B(A):
+            b = 1
+        C = B()
+        C.a = 2
+        self.assertIs(I.getparent(C, 'a'), C)
+        self.assertIs(I.getparent(B, 'a'), A)
+        self.assertIs(I.getparent(B, 'b'), B)
+        self.assertIs(I.getparent(B, '__hash__'), object)
+        with self.assertRaises(AttributeError):
+            I.getparent(C, 'd')
+
+class Test_getparents(I.unittest.TestCase):
+    def test(self):
+        class A:
+            a = 1
+        class B(A):
+            b = 1
+        C = B()
+        C.a = 2
+        self.assertEqual(I.getparents(C), (C, B, A, object))
+        self.assertEqual(I.getparents(B), (B, A, object))
+        self.assertEqual(I.getparents(hex), (hex, hex.__class__, object))
+        self.assertEqual(I.getparents(object), (object,))
+        self.assertEqual(I.getparents(type), (type, object,))
+
+if __name__ == '__main__':
+    ttr = I.unittest.TextTestRunner(tb_locals=True)
+    I.unittest.main(testRunner=ttr, verbosity=2)
